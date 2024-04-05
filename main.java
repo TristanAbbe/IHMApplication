80e018adbba35431f27c98c88a12bcdd0c051143
Hqsake <tristan.abbe@hotmail.fr>
2024-04-05 11:18:15 +0200

    ajout modification main et librarie

--- main.java
+++ main.java
@@ -1,28 +1,47 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
-public class main {
+public class MainController {
 
     @FXML
-    private void AjoutStage() {
-        // Action à effectuer lors du clic sur le bouton "Ajouter Stage"
+    private Button ajoutStageButton;
+
+    @FXML
+    private Button listeStageButton;
+
+    @FXML
+    private Button aideButton;
+
+    @FXML
+    private Button quitterButton;
+
+    @FXML
+    private void initialize() {
+        // Vous pouvez ajouter des initialisations ici si nécessaire
+    }
+
+    @FXML
+    private void handleAjoutStage() {
         System.out.println("Bouton Ajouter Stage cliqué");
+        // Ajoutez votre code pour l'action d'ajout de stage ici
     }
 
     @FXML
-    private void ListeStage() {
-        // Action à effectuer lors du clic sur le bouton "Liste Stage"
+    private void handleListeStage() {
         System.out.println("Bouton Liste Stage cliqué");
+        // Ajoutez votre code pour l'action de liste de stage ici
     }
 
     @FXML
-    private void Aide() {
-        // Action à effectuer lors du clic sur le bouton "Aide"
+    private void handleAide() {
         System.out.println("Bouton Aide cliqué");
+        // Ajoutez votre code pour l'action d'aide ici
     }
 
     @FXML
-    private void Quitter() {
-        // Action à effectuer lors du clic sur le bouton "Quit"
+    private void handleQuitter() {
         System.out.println("Bouton Quit cliqué");
+        // Ajoutez votre code pour l'action de quitter ici
     }
 }
