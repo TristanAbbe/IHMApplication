59bae7e1625cb47d1a3e4610c0989938432157f5
Hqsake <tristan.abbe@hotmail.fr>
2024-04-05 10:54:44 +0200

    ajout modification main et librarie

--- main.java
+++ main.java
@@ -1,2 +1,28 @@
+import javafx.fxml.FXML;
+
 public class main {
+
+    @FXML
+    private void AjoutStage() {
+        // Action à effectuer lors du clic sur le bouton "Ajouter Stage"
+        System.out.println("Bouton Ajouter Stage cliqué");
+    }
+
+    @FXML
+    private void ListeStage() {
+        // Action à effectuer lors du clic sur le bouton "Liste Stage"
+        System.out.println("Bouton Liste Stage cliqué");
+    }
+
+    @FXML
+    private void Aide() {
+        // Action à effectuer lors du clic sur le bouton "Aide"
+        System.out.println("Bouton Aide cliqué");
+    }
+
+    @FXML
+    private void Quitter() {
+        // Action à effectuer lors du clic sur le bouton "Quit"
+        System.out.println("Bouton Quit cliqué");
+    }
 }
