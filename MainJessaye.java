33ab45f3b7c2fb9d7b62cbbdc8e8c98bcb97dbb0
Mélany LEFEVRE--ROMY <melany.lefevreromy@gmail.com>
2024-04-06 17:26:24 +0200

    ListeStage en cours

--- MainJessaye.java
+++ MainJessaye.java
@@ -9,7 +9,7 @@ public class MainJessaye extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("popup_annultion.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("popup_annulation.fxml"));
             primaryStage.setTitle("Hello World");
             primaryStage.setScene(new Scene(root, 800, 600)); // Spécifiez une taille de scène
             primaryStage.show();
